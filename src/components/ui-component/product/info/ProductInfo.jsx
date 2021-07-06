9b2a2d8d9d62e0fe25d420fa9e2ffb9211ed6275
Jose Rodriguez <54864256+joalrope@users.noreply.github.com>
2021-07-06 13:54:20 -0400

    Product-card carousel add

--- src/components/ui-component/product/info/ProductInfo.jsx
+++ src/components/ui-component/product/info/ProductInfo.jsx
@@ -1,28 +1,20 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { Card, Avatar, Image, Collapse, Tooltip } from 'antd';
-import { CloseOutlined, CheckCircleTwoTone } from '@ant-design/icons';
+import { Card, Avatar, Image, Tooltip, Carousel } from 'antd';
+import { CloseOutlined, LeftOutlined, RightOutlined } from '@ant-design/icons';
 import { productClearActive } from '../../../../actions/products';
-//import cat from '../../../../assets/images/cat.png';
 import './product-info.scss';
 
 const { Meta } = Card;
-const { Panel } = Collapse;
 
 export const ProductInfo = ({ product, setProductForSale }) => {
   const dispatch = useDispatch();
-  const dataSource = product;
+  const carouselRef = useRef('');
 
   const clearActiveProduc = () => {
     dispatch(productClearActive());
   };
-
-  const genExtra = () => (
-    <Tooltip placement='topLeft' title='Detalles'>
-      <CheckCircleTwoTone />
-    </Tooltip>
-  );
 
   const handleTrademarkClick = (trademark, salePrice) => {
     const selectedProduct = {
@@ -37,47 +29,69 @@ export const ProductInfo = ({ product, setProductForSale }) => {
     setProductForSale(selectedProduct);
   };
 
-  const action = dataSource.details.map((item) => {
-    return (
-      <div key={item.trademark}>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <Tooltip placement='topLeft' title='Clic para agregar a la venta'>
-            <Avatar
-              src={<img src={`${process.env.PUBLIC_URL}/assets/icons/${item.trademark}.png`} alt={item.trademark} />}
-              size={24}
-              onClick={() => handleTrademarkClick(item.trademark, item.salePrice)}
-            />
-          </Tooltip>
-        </div>
+  const onChangeCarousel = (e) => {
+    console.log(e);
+  };
 
-        <Collapse bordered={false} style={{ backgroundColor: 'transparent' }}>
-          <Panel showArrow={false} header={<span>{`$${item.salePrice}`}</span>} key='1' extra={genExtra()}>
-            {Object.values(item.stock).map((loc) => (
-              <div key={loc.location} className='stock'>
-                <p>{`Cant: ${loc.qty}`}</p>
-                <p>{`Loc : ${loc.location}`}</p>
-              </div>
-            ))}
-          </Panel>
-        </Collapse>
-      </div>
-    );
-  });
+  const handleClickPrev = () => {
+    console.log('uuuuuuuuu');
+    carouselRef.current.prev();
+  };
 
-  const actions = [];
-  actions.push(action);
+  const handleClickNext = () => {
+    carouselRef.current.next();
+  };
+
+  const settings = {
+    prevArrow: <LeftOutlined onClick={handleClickPrev} />,
+    nextArrow: <RightOutlined onClick={handleClickNext} />,
+  };
 
   return (
     <div className='product-info__container'>
       <Card
         style={{ width: 150 }}
         cover={<Image alt='example' src='https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png' />}
-        actions={actions}
+        //actions={actions}
         size='small'
-        title={dataSource.code}
+        title={product.code}
         extra={<CloseOutlined onClick={clearActiveProduc} />}
       >
-        <Meta description={<span>{dataSource.title}</span>} />
+        <Meta description={<span>{product.title}</span>} />
+        <Carousel
+          myRef={carouselRef}
+          className='--product-card__carousel'
+          arrows
+          {...settings}
+          dots={false}
+          afterChange={onChangeCarousel}
+        >
+          {Object.values(product.details).map((item) => {
+            return (
+              <div key={item.trademark} className='--product-card__details'>
+                <Tooltip key={item.trademark} placement='topLeft' title='Clic para agregar a la venta'>
+                  <Avatar
+                    className='--product-card__brand-avatar'
+                    src={
+                      <img src={`${process.env.PUBLIC_URL}/assets/icons/${item.trademark}.png`} alt={item.trademark} />
+                    }
+                    //size={48}
+                    onClick={() => handleTrademarkClick(item.trademark, item.salePrice)}
+                  />
+                  {Object.values(item.stock).map((stock) => {
+                    return (
+                      <div key={1} className='--product-card__stock'>
+                        <p>{`Precio: $${item.salePrice}`}</p>
+                        <p>{`Cantidad: ${stock.qty}`}</p>
+                        <p>{`Locación : ${stock.location}`}</p>
+                      </div>
+                    );
+                  })}
+                </Tooltip>
+              </div>
+            );
+          })}
+        </Carousel>
       </Card>
     </div>
   );
