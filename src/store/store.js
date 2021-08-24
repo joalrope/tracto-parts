80b02868b216fb38e9465df4ad71b78a818493b6
Jose Rodriguez <54864256+joalrope@users.noreply.github.com>
2021-08-24 12:51:02 -0400

    store update for deploy 24ago21

--- src/store/store.js
+++ src/store/store.js
@@ -4,11 +4,11 @@ import thunk from 'redux-thunk';
 import { rootReducer } from '../reducers/rootReducer';
 
 const composeEnhancers =
-  (typeof window !== 'undefined' &&
+  /*   (typeof window !== 'undefined' &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
       trace: true,
       traceLimit: 5,
-    })) ||
+    })) || */
   compose;
 
 export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
