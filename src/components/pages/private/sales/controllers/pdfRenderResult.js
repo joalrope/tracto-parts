417962138cff6c937e3cd024f1ba0448e08dfb31
Jose Rodriguez <54864256+joalrope@users.noreply.github.com>
2021-08-25 11:40:18 -0400

    deploy II 25ago21

--- src/components/pages/private/sales/controllers/pdfRenderResult.js
+++ src/components/pages/private/sales/controllers/pdfRenderResult.js
@@ -1,11 +1,10 @@
 import Swal from 'sweetalert2';
-//import { fetchWithToken } from '../../../../../helpers/fetch';
-//import { controlNumber } from '../controllers/getTransactionInfo';
+import { createBrowserHistory } from 'history';
 import { store } from '../../../../../store/store';
 import { customerClearActive } from '../../../../../actions/customers';
 import { clearProductsForSale, productClearActive } from '../../../../../actions/products';
 
-//const urlNextTransaction = '/transaction/nextTransaction';
+const history = createBrowserHistory();
 
 export const msgWhenUnmounting = (controlNumber) => {
   Swal.fire({
@@ -16,10 +15,10 @@ export const msgWhenUnmounting = (controlNumber) => {
     denyButtonText: `No`,
   }).then(async (result) => {
     if (result.isConfirmed) {
-      //await fetchWithToken(urlNextTransaction, {}, 'PATCH');
       store.dispatch(customerClearActive());
       store.dispatch(productClearActive());
       store.dispatch(clearProductsForSale());
+      history.push('/app/sales');
     } else {
       result.isConfirmed = false;
     }
