acd1425d9d5595a77c5807850e689e4890888617
Jose Rodriguez <54864256+joalrope@users.noreply.github.com>
2021-10-12 11:04:24 -0400

    Login.jsx fix

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,7 +1,7 @@
 import { NotFound } from './public/notfound/NotFound';
 import { About } from './public/about/About';
 import { Forgot } from './public/auth/forgot/Forgot';
-import { Login } from './public/auth/login/Login';
+import { Login } from './public/auth/login/Login1';
 import { Register } from './public/auth/register/Register1.jsx';
 import { Contact } from './public/contact/Contact';
 import { Home } from './public/home/Home';
